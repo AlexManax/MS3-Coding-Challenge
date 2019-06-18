e17acc60e4f29c6d76059892b6aef41f53d90447
Alex <AlexManax@gmail.com>
2019-06-18 11:07:41 -0700

    Task is done: Comments added.

--- src/main/java/DBservice.java
+++ src/main/java/DBservice.java
@@ -6,21 +6,21 @@ public class DBservice {
     private Connection connection = null;
     private PreparedStatement pstmt = null;
 
-    private Connection connect() {
+    private Connection connect() { //establish DB connection + config
         try {
             if (connection != null && !connection.isClosed()) return connection;
             else
                 try {
                     SQLiteConfig config = new SQLiteConfig();
 
-                    config.setPragma(SQLiteConfig.Pragma.SYNCHRONOUS, "OFF");
-                    config.setPragma(SQLiteConfig.Pragma.JOURNAL_MODE, "Memory");
+                    config.setPragma(SQLiteConfig.Pragma.SYNCHRONOUS, "OFF"); //SQLite config - don't wait complete write operation
+                    config.setPragma(SQLiteConfig.Pragma.JOURNAL_MODE, "Memory"); //Journal in memory - dramatically increase productivity
 
                     connection = DriverManager.getConnection("jdbc:sqlite::memory", config.toProperties());
 //                    connection = DriverManager.getConnection("jdbc:sqlite:identifier.sqlite",config.toProperties());
 
                 } catch (SQLException e) {
-                    System.out.println(e.getMessage());
+                    e.printStackTrace();
                 }
         } catch (SQLException e) {
             e.printStackTrace();
@@ -28,11 +28,11 @@ public class DBservice {
         return connection;
     }
 
-    public void initDB(String[] columnsNames) {
+    public void initDB(String[] columnsNames) { //creates DB table
         String sql = null;
 
         try {
-            sql = String.format(
+            sql = String.format( //define columns names from first record
 //                            "DELETE FROM userData;\n" +
 //                            "VACUUM;\n" +
                     "CREATE TABLE IF NOT EXISTS userData(\n" +
@@ -62,10 +62,10 @@ public class DBservice {
         }
     }
 
-    public void insert(String[] record) {
-        getPrepareStatement();
+    public void insert(String[] record) { //insert record in DB
+        getPrepareStatement(); //init singleton PrepareStatement
         try {
-            for (int i = 1; i < record.length + 1; i++) {
+            for (int i = 1; i < record.length + 1; i++) { //add fields from record to PrepareStatement
                 pstmt.setString(i, record[i - 1]);
             }
             pstmt.executeUpdate();
@@ -75,21 +75,20 @@ public class DBservice {
     }
 
     private void getPrepareStatement() {
-        if (pstmt != null) return;
-        else {
+        if (pstmt == null) {
             try {
-                pstmt = connection.prepareStatement("INSERT INTO userData VALUES(?,?,?,?,?,?,?,?,?,?)");
+                pstmt = connection.prepareStatement("INSERT INTO userData VALUES(?,?,?,?,?,?,?,?,?,?)"); //PS with undefined columns in order to admit dynamic columns names functionality.
             } catch (SQLException e) {
                 e.printStackTrace();
             }
         }
     }
-    public void closeConnection(){
+    public void closeConnection(){ //close connection to DB
         try {
             connection.close();
         } catch (SQLException e) {
             e.printStackTrace();
-        }
+        } //I got fun, hope you too =) Thx.
     }
 }
 
--- src/main/java/ParserCSV.java
+++ src/main/java/ParserCSV.java
@@ -9,55 +9,58 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class ParserCSV {
-    public static void main(String[] args) {
-        Date start = new Date();
-        DBservice dBservice = new DBservice();
+    public static void main(String[] args) { //args receiving 2 params: path to CSV file and path to output directory
+        Date start = new Date();//Used for time test
+        DBservice dBservice = new DBservice();//Used for access to DB methods
         int countGoodRecords = 0;
         int countBadRecords = 0;
-        String csvFilePath = args[1] + "/bad-data-" + new SimpleDateFormat("yyyyMMdd-HHmmss").format(new Date()) + ".csv";
+        String csvFilePath = args[1] + "/bad-data-" + new SimpleDateFormat("yyyyMMdd-HHmmss").format(new Date()) + ".csv";//Defined path to log CSV file
         try {
-            Files.createFile(Paths.get(csvFilePath));
+            Files.createFile(Paths.get(csvFilePath)); //Created the log CSV file, assumed it does not exist, even thought it will be catches
         } catch (IOException e) {
             e.printStackTrace();
         }
+        //try with resources is great closing procedure for java 7+
+        //Cover the FR/FW to Buffer to optimize reading/writing
         try (CSVReader reader = new CSVReader(new BufferedReader(new FileReader(args[0])));
              CSVWriter writer = new CSVWriter(new BufferedWriter(new FileWriter(csvFilePath)))
         ) {
-            System.out.println(args[1]);
-            String[] line;
-            boolean triger = true;
-            while ((line = reader.readNext()) != null) {
-                if (triger) {
-                    if (line.length == 10) {
-                        dBservice.initDB(line);
-                        triger = false;
-                        countGoodRecords++;
+            String[] line; //used to save fields from a record
+            boolean trigger = true; //trigger is used one time (decided to use it in order to optimize the speed, probably not the best solution)
+
+            while ((line = reader.readNext()) != null) { //started the file reader cycle
+                if (trigger) { //one time use only for defining first record with names of columns
+                    if (line.length == 10) {//double check if it's a good record
+                        dBservice.initDB(line); //creates DB table
+                        trigger = false;
+                        countGoodRecords++; //adjust counter
                     }
-                } else if (line.length != 10) {
-                    writer.writeNext(line);
-                    countBadRecords++;
-                } else {
-                    dBservice.insert(line);
-                    countGoodRecords++;
+                } else if (line.length != 10) { //check if record contains not 10 columns
+                    writer.writeNext(line); //write record to file
+                    countBadRecords++; //adjust counter
+                } else { // only good records
+                    dBservice.insert(line); //insert in DB the record
+                    countGoodRecords++; //adjust counter
                 }
             }
-            dBservice.closeConnection();
+            dBservice.closeConnection(); //close DB connection. Unusual solution. Decided to do it in order to optimize insert operations, It will not initialize the connection on each insert operation.
         } catch (Exception e) {
             e.printStackTrace();
-            dBservice.closeConnection();
+            dBservice.closeConnection(); //close DB connection if something is wrong =)
         }
-        writeLog(args[1], countGoodRecords, countBadRecords);
+        writeLog(args[1], countGoodRecords, countBadRecords); //it writes the log in file
         System.out.println(new Date().getTime() - start.getTime());
     }
 
     private static void writeLog(String arg, int countGood, int countBad) {
         String logFilePath = arg + "/log.log";
-        String log = countGood + countBad + " records received\n" + countGood + " records successful\n" + countBad + " records failed\n ****************************************\n";
+        String log = countGood + countBad + " records received\n" + countGood + " records successful\n"
+                        + countBad + " records failed\n ****************************************\n";
         try {
             if (Files.exists(Paths.get(logFilePath))) {
-                Files.write(Paths.get(logFilePath), log.getBytes(), StandardOpenOption.APPEND);
+                Files.write(Paths.get(logFilePath), log.getBytes(), StandardOpenOption.APPEND); //append if file exist
             } else {
-                Files.write(Paths.get(logFilePath), log.getBytes(), StandardOpenOption.CREATE);
+                Files.write(Paths.get(logFilePath), log.getBytes(), StandardOpenOption.CREATE); //create if file is not exist
             }
         } catch (IOException e) {
             e.printStackTrace();
